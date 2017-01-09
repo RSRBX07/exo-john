8089461d306e6303aa474536ee211983ab4664a7
monteil <john_monteil@hotmail.com>
2017-01-09 15:08:59 +0100

    mon age en seconde

---
+++ numbers.rb
@@ -0,0 +1,12 @@
+#exercice sur les nombres
+#1 calculer le nbre de sec ds 1 année
+annee = 365*24*360
+print "nombre de seconde dans une annee:#{annee}"
+#afficher votre age en seconde""
+age= 54*364*24*360
+print "afficher votre age en seconde  age:#{age}"
+now='Time.now'
+birthTime = Time.new "21/11/1962"
+agesecond = now - birthTime
+
+
