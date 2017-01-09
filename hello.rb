4d2ff31a7a4bf0a115531dc62361ec84b58e8d98
monteil <john_monteil@hotmail.com>
2017-01-09 15:08:30 +0100

    fin hello.rb

--- hello.rb
+++ hello.rb
@@ -4,3 +4,34 @@ puts"comment vas tu ?"
 puts"quel est votre nom"
 Name = gets
 puts "hello jean robert"
+puts "hello jean robert"
+puts "hello jean robert"
+puts " hello jr"
+3.times do puts "hello # {name}" end
+
+name = "pierre" 
+"pierre".capitalize
+"Pierre" .upcase
+"pierre".lenght
+name.reverse!
+full_name = "toto
+self.puts ["coucou"]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
