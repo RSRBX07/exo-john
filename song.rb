0dd006822468cadc25c6caf5b4ad461ff322c498
monteil <john_monteil@hotmail.com>
2017-01-10 16:29:27 +0100

    Travail en cours de song.rb

---
+++ song.rb
@@ -0,0 +1,25 @@
+    = begin
+     Ma poule n'a plus qu'vingt-neuf poussins (bis)
+Elle en a eu trente
+Allongeons la jambe
+refrain - Allongeons la jambe, la jambe
+Car la route est longue!
+Allongeons la jambe, la jambe
+Car la route est longue
+Ma poule n'a plus qu'vingt-huit poussins...
+Ma poule n'a plus qu'un seul poussin...
+Ma poule n'a plus aucun poussin
+Elle en a eu trente
+Allongeons la jambe
+refrain - Allongeons la jambe, la jambe
+Car la route est longue!
+Allongeons la jambe, la jambe
+Car la route est longue 
+=end
+
+puts " Ma poule n'a plus qu'vingt-neuf poussins"
+p= (29 poussins)
+p = (29 + 1 poussins)
+puts = "allongeons la jambe"
+
+
