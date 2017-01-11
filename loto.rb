a050f4895a37586197502d7282555425146cb9c3
monteil <john_monteil@hotmail.com>
2017-01-11 10:54:31 +0100

    struture  du programme du loto

--- loto.rb
+++ loto.rb
@@ -4,26 +4,32 @@ puts "demander une grille de jeu"
 # affichage du montant de la cagnotte
 #entre 100 et 500
 # le vendredi 13, la cagnotte est de 2 millions
-le vendredi_13? = false
-if vendredi_13? 
-    cagnotte = 2.000.000
+is_vendredi_13 = false
+if is_vendredi_13 
+    cagnotte = 2,000,000
 else
-    cagnotte = 100.000
- end 
+    cagnotte = 100,000
+end 
+
+#valider la grille
+
+puts " entrer 5 chiffres? "
+
+
+available_balls  = (1..45).to_a
+ticketloto = available_balls.shuffle.take 5 
+puts "afficher le ticketloto"
+
+
+#tirage aleatoire de 5 nbres
  puts "le montant de la cagnotte du jour est de #{cagnote}"
  available_balls = (1..45).to_a#shuffle balls and take 5
  picked_balls = available_balls.shuffle.take 5
 
- puts " le tirage du jour est : #{picked_balls.sort}"
+ puts " le tirage du jour est : #{picked_balls.sort}"        
 
 
 #afficher si gagne ou perdu
-puts " entrer 5 chiffres"
-available_balls  = (1..5).to_a#shuffle balls and take 5
-picked_balls = available_balls.shuffle.take 5
 if trois nombres gagnant affiche gagne? = true
     else affiche perdu = false
         end
-
-        
-
