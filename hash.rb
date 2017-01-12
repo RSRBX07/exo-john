bb492f3689ecc288d8e60996846b7caff09b4c88
monteil <john_monteil@hotmail.com>
2017-01-12 11:28:56 +0100

    excercice code postaux

--- hash.rb
+++ hash.rb
@@ -2,3 +2,11 @@ meta = {"place" => "roubaix" , "author" => "jean" , "date" => "2017-01",
 "nom"=>"place", "ville"=> "roubaix", "prenom"=>"jean", "annee"=>"date"}
 puts meta
 
+
+ville = meta['ville']
+
+puts "aller à #{ville}"
+
+code_postaux = {"roubaix"=>59100,"ronchin"=>59790,"lille"=>59000}
+puts "afficher  #{code_postaux['roubaix']}"
+
