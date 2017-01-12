8ca920a127a0c299066f2b363783c948762d48b0
monteil <john_monteil@hotmail.com>
2017-01-12 15:31:59 +0100

    in_french_num

--- french_number.rb
+++ french_number.rb
@@ -1,28 +1,22 @@
-def in_french num
-    #a(accepte de 0 à 9)
-case num
-when 0 
-    "zero"
-when"1"
-    "un"
-when"2"
-    "zero"
-when"3"
-    "quatre"
-when"5"
-    "cinq"
-when"6"
-    "six"
-when"7"
-    "sept"
-when"8"
-    "huit"
-when"9"
-    "neuf"
-else
-         nil
-    end
 
 
-    end
-   puts in_french 5
+def in_french_num     index
+
+    french_string = ["zero","un","deux","trois","quatre","cing","six","sept","huit","neuf" ]
+
+    french_string[index]
+
+end
+
+puts in_french_num  1
+puts in_french_num  6
+puts in_french_num  2
+
+
+
+
+
+
+
+
+
