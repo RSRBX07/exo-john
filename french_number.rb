1fa0a70a5064b8edc4fbeb3735ba17eb8efac82d
monteil <john_monteil@hotmail.com>
2017-01-12 14:31:39 +0100

    ex hash.rb

--- french_number.rb
+++ french_number.rb
@@ -1,2 +1,34 @@
-#ecrire une methode qui prend une instance de Fixnum en argument (accepte de 0 à 9s)
-#qui retourne la chaine de caracteredu nombre ecrit en toutes lettres
+def in_french num
+    #a(accepte de 0 à 9)
+case num
+when 0 
+    "zero"
+    when"1"
+    "un"
+    when"2"
+    "zero"
+     when"3"
+    "quatre"
+     when"5"
+    "cinq"
+     when"6"
+    "six"
+     when"7"
+    "sept"
+     when"8"
+    "huit"
+     when"9"
+    "neuf"
+     else
+         nil
+    end
+
+
+   #qui retourne la chaine de caractere du nombre ecrit en toutes lettres
+   french_string_number = "zero"
+
+     
+
+    end
+
+   puts in_french 5
