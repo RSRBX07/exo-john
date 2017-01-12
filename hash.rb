0483dec2b282c0259d11534f1552e70516ed0c9d
monteil <john_monteil@hotmail.com>
2017-01-12 13:51:24 +0100

    ex code postaux

--- hash.rb
+++ hash.rb
@@ -3,10 +3,14 @@ meta = {"place" => "roubaix" , "author" => "jean" , "date" => "2017-01",
 puts meta
 
 
+
 ville = meta['ville']
 
 puts "aller à #{ville}"
 
 code_postaux = {"roubaix"=>59100,"ronchin"=>59790,"lille"=>59000}
 puts "afficher  #{code_postaux['roubaix']}"
+
+zip_codes = {lille:59000, paris:75000, brest:35000, montpellier:34000}
+puts "afficher  #{code_postaux['lille']}"
 
