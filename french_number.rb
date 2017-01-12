96ecf0e14fcdf3c7aa3977dbc15fe0da2d64bc3d
monteil <john_monteil@hotmail.com>
2017-01-12 14:35:55 +0100

    exercice french_number

--- french_number.rb
+++ french_number.rb
@@ -3,32 +3,26 @@ def in_french num
 case num
 when 0 
     "zero"
-    when"1"
+when"1"
     "un"
-    when"2"
+when"2"
     "zero"
-     when"3"
+when"3"
     "quatre"
-     when"5"
+when"5"
     "cinq"
-     when"6"
+when"6"
     "six"
-     when"7"
+when"7"
     "sept"
-     when"8"
+when"8"
     "huit"
-     when"9"
+when"9"
     "neuf"
-     else
+else
          nil
     end
 
 
-   #qui retourne la chaine de caractere du nombre ecrit en toutes lettres
-   french_string_number = "zero"
-
-     
-
     end
-
    puts in_french 5
