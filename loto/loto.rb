946d508676a225bd5220c5e09cd738a624487206
monteil <john_monteil@hotmail.com>
2017-01-18 11:55:29 +0100

    correction methode draw

--- loto/loto.rb
+++ loto/loto.rb
@@ -59,7 +59,7 @@ class Loto
     available_balls = (1..45).to_a
     # shuffle balls and take 5
     # @picked_balls ||= available_balls.shuffle.take(5)
-    @picked_balls = available_balls.shuffle.take(5)
+    @picked_balls = @picked_balls || available_balls.shuffle.take(5)
     puts "Le tirage du jour est : #{@picked_balls.sort}" 
     @picked_balls
   end
