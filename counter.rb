b38869c49d58952472e76372ee017830d7129f5b
monteil <john_monteil@hotmail.com>
2017-01-19 16:44:20 +0100

    counter.rb

--- counter.rb
+++ counter.rb
@@ -4,13 +4,13 @@ class Counter
     dirname + 'counter.txt'
   end
 
-  def add_one
+  def add_plane
     @read_value = value + 1
     File.open(Counter.get_filename, "w") do |counter_file|
       counter_file.write @read_value
   end
 
-  def value
+  def value_plane
     @read_value ||= File.open Counter.get_filename, "r" do |counter_file|
       counter_file.each_line { |line| return line.to_i }
   end  
