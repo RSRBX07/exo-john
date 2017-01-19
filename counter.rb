fa072cdb6478c09eea05b8a1b68521363b514544
monteil <john_monteil@hotmail.com>
2017-01-19 16:16:19 +0100

    created counter.rb

---
+++ counter.rb
@@ -0,0 +1,18 @@
+class Counter
+  def self.get_filename
+    dirname = File.join(File.dirname(__FILE__), '../tmp/')
+    dirname + 'counter.txt'
+  end
+
+  def add_one
+    @read_value = value + 1
+    File.open(Counter.get_filename, "w") do |counter_file|
+      counter_file.write @read_value
+  end
+
+  def value
+    @read_value ||= File.open Counter.get_filename, "r" do |counter_file|
+      counter_file.each_line { |line| return line.to_i }
+  end  
+
+end
