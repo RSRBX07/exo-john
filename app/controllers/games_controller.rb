58b6e7fd0cb1bfd22615c5622bed65e6556f2648
monteil <john_monteil@hotmail.com>
2017-01-23 17:08:07 +0100

    migrate game add_players

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -10,6 +10,7 @@ class GamesController < ApplicationController
   # GET /games/1
   # GET /games/1.json
   def show
+   
   end
 
   # GET /games/new
--- app/models/game.rb
+++ app/models/game.rb
@@ -1,20 +1,13 @@
 class Game < ApplicationRecord
 
   def add_player
-    @players += 1
+    @players ||=0
+    @players += 1 
   end
 
   def remove_player
-     raise RuntimeError.new "No more player to be removed" @players if @players <= 0
+     raise RuntimeError.new "No more player to be removed" if @players <= 0
      @players -= 1
   end
 
-  private
-
-  def initialize
-    @players = 0
-  end
-
-  
-
-end
+ end
